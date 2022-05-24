aaf8bb0c86cfefd1456bf4a21c601fd50d9308d3
Иван Мазунин <91690338+IvanMazunin@users.noreply.github.com>
2022-05-24 23:13:50 +0500

    Update MyForm.cpp

--- AppКалькулятор/MyForm.cpp
+++ AppКалькулятор/MyForm.cpp
@@ -8,6 +8,6 @@ void main(array<String^>^ arg) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 
-	Калькулятор::MyForm form;
+	РљР°Р»СЊРєСѓР»СЏС‚РѕСЂ::MyForm form;
 	Application::Run(% form);
 }
